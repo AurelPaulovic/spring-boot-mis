38adbae4b1f6ed42d236f7bf007f248f3edfd94e
Aurel Paulovic <aurel.paulovic@gmail.com>
2015-03-19 13:31:11 +0100

    basic bootstrap

---
+++ src/main/java/sk/anext/msi/SpringBootMsiApplication.java
@@ -0,0 +1,13 @@
+package sk.anext.msi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class SpringBootMsiApplication extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMsiApplication.class, args);
+    }
+}
---
+++ src/test/java/sk/anext/msi/SpringBootMsiApplicationTests.java
@@ -0,0 +1,18 @@
+package sk.anext.msi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootMsiApplication.class)
+@WebAppConfiguration
+public class SpringBootMsiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
