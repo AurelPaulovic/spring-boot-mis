91d6ded0a3f1ce16818f829d6df6c5fa3af13f26
Aurel Paulovic <aurel.paulovic@gmail.com>
2015-03-23 10:34:44 +0100

    added a reusable component with a simple counter

---
+++ src/main/java/sk/anext/msi/ui/component/SessionPanel.java
@@ -0,0 +1,25 @@
+package sk.anext.msi.ui.component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component("SessionPanel")
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class SessionPanel {
+    private final AtomicInteger userAddedCount = new AtomicInteger(0);
+
+    public Integer getUserAddedCount() {
+        return userAddedCount.get();
+    }
+
+    public void setUserAddedCount(Integer userAddedCount) {
+        this.userAddedCount.set(userAddedCount);
+    }
+    
+    public void incrementAddedCount() {
+        this.userAddedCount.addAndGet(1);
+    }
+}
--- src/main/java/sk/anext/msi/ui/controller/UserController.java
+++ src/main/java/sk/anext/msi/ui/controller/UserController.java
@@ -4,19 +4,25 @@ import javax.validation.Valid;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import sk.anext.msi.ui.component.SessionPanel;
 import sk.anext.msi.ui.form.UserForm;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     private static final Log log = LogFactory.getLog(UserController.class);
+    
+    @Autowired
+    private SessionPanel sessionPanel;
     
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String showAddForm(Model model) {
@@ -25,13 +31,14 @@ public class UserController {
     }
     
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String submitAddForm(@Valid @ModelAttribute("user") UserForm user, BindingResult bindingResult) {
+    public String submitAddForm(@Valid @ModelAttribute("user") UserForm user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             log.info("got some errors");
             return "user/form";
         }
         
-        log.info("no errors");
+        sessionPanel.incrementAddedCount();
+        redirectAttributes.addFlashAttribute("message", "User was successfully added!");
         return "redirect:/user/add";
     }
 }
